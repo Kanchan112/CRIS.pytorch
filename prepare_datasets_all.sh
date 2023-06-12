20294ad4928933f19bb47d265046878bd2d29277
Kanchan112 <poudel.kanchan38@gmail.com>
2023-06-12 16:47:02 +0545

    exp

--- prepare_datasets_all.sh
+++ prepare_datasets_all.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 dataset_name='kvasir_polyp_80_10_10'
-version='_testxxxxx'
+version='allp'
 
 source ./.venv/bin/activate
 cd datasets
-python /mnt/Enterprise/kanchan/CRIS.pytorch/tools/folder2lmdb.py -j anns_old/kvasir_polyp_80_10_10/train.json -i images/$dataset_name/ -m masks/$dataset_name -o lmdb_old/$dataset_name$version
-python /mnt/Enterprise/kanchan/CRIS.pytorch/tools/folder2lmdb.py -j anns_old/kvasir_polyp_80_10_10/val.json -i images/$dataset_name/ -m masks/$dataset_name -o lmdb_old/$dataset_name$version
-python /mnt/Enterprise/kanchan/CRIS.pytorch/tools/folder2lmdb.py -j anns_old/kvasir_polyp_80_10_10/testA.json -i images/$dataset_name/ -m masks/$dataset_name -o lmdb_old/$dataset_name$version
-python /mnt/Enterprise/kanchan/CRIS.pytorch/tools/folder2lmdb.py -j anns_old/kvasir_polyp_80_10_10/testB.json -i images/$dataset_name/ -m masks/$dataset_name -o lmdb_old/$dataset_name$version
+python ../tools/folder2lmdb.py -j /mnt/Enterprise/miccai_2023_CRIS/CRIS.pytorch/datasets/anns/kvasir_polyp_80_10_10/train.json -i images/$dataset_name/ -m masks/$dataset_name -o lmdb/$dataset_name$version
+python ../tools/folder2lmdb.py -j /mnt/Enterprise/miccai_2023_CRIS/CRIS.pytorch/datasets/anns/kvasir_polyp_80_10_10/val.json -i images/$dataset_name/ -m masks/$dataset_name -o lmdb/$dataset_name$version
+python ../tools/folder2lmdb.py -j /mnt/Enterprise/miccai_2023_CRIS/CRIS.pytorch/datasets/anns/kvasir_polyp_80_10_10/testA.json -i images/$dataset_name/ -m masks/$dataset_name -o lmdb/$dataset_name$version
+python ../tools/folder2lmdb.py -j /mnt/Enterprise/miccai_2023_CRIS/CRIS.pytorch/datasets/anns/kvasir_polyp_80_10_10/testB.json -i images/$dataset_name/ -m masks/$dataset_name -o lmdb/$dataset_name$version
 
