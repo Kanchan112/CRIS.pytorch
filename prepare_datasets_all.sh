36d5ab57df4a6d7369d308731c69089b234e124f
Kanchan112 <poudel.kanchan38@gmail.com>
2023-08-29 00:20:57 +0545

    Update script for lmdb generation

--- prepare_datasets_all.sh
+++ prepare_datasets_all.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-dataset_name='kvasir_polyp_80_10_10'
-version='allp'
-
-source ./.venv/bin/activate
+source /mnt/Enterprise/miccai_2023_CRIS/CRIS.pytorch/.venv/bin/activate
 cd datasets
-python ../tools/folder2lmdb.py -j /mnt/Enterprise/miccai_2023_CRIS/CRIS.pytorch/datasets/anns/kvasir_polyp_80_10_10/train.json -i images/$dataset_name/ -m masks/$dataset_name -o lmdb/$dataset_name$version
-python ../tools/folder2lmdb.py -j /mnt/Enterprise/miccai_2023_CRIS/CRIS.pytorch/datasets/anns/kvasir_polyp_80_10_10/val.json -i images/$dataset_name/ -m masks/$dataset_name -o lmdb/$dataset_name$version
-python ../tools/folder2lmdb.py -j /mnt/Enterprise/miccai_2023_CRIS/CRIS.pytorch/datasets/anns/kvasir_polyp_80_10_10/testA.json -i images/$dataset_name/ -m masks/$dataset_name -o lmdb/$dataset_name$version
-python ../tools/folder2lmdb.py -j /mnt/Enterprise/miccai_2023_CRIS/CRIS.pytorch/datasets/anns/kvasir_polyp_80_10_10/testB.json -i images/$dataset_name/ -m masks/$dataset_name -o lmdb/$dataset_name$version
 
+for dataset_name in 'bkai_polyp_80_10_10' 'kvasir_polyp_80_10_10' 'dfu-2022_80_10_10' 'clinicdb_polyp_80_10_10' 'isic_90_10_d' 'camus_80_10_10' 'chexlocalize_80_10_10' 'busi_80_10_10'
+do 
+    python ./CRIS.pytorch/tools/folder2lmdb.py -j anns/$dataset_name/train.json -i images/$dataset_name/ -m masks/$dataset_name -o lmdb/$dataset_name
+    python ./CRIS.pytorch/tools/folder2lmdb.py -j anns/$dataset_name/val.json -i images/$dataset_name/ -m masks/$dataset_name -o lmdb/$dataset_name
+    python ./CRIS.pytorch/tools/folder2lmdb.py -j anns/$dataset_name/testA.json -i images/$dataset_name/ -m masks/$dataset_name -o lmdb/$dataset_name
+    python ./CRIS.pytorch/tools/folder2lmdb.py -j anns/$dataset_name/testB.json -i images/$dataset_name/ -m masks/$dataset_name -o lmdb/$dataset_name
+done
